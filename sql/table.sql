c3716aaf1feebc7b87ee703c2d1b6b26ce9d62ad
min <gmlee@sunnysidegames.co.kr>
2019-08-07 14:31:07 +0900

    ..

--- sql/table.sql
+++ sql/table.sql
@@ -62,19 +62,19 @@ VALUES(0, 1, GETDATE())
 -- ==========================================================================================
 CREATE TABLE [dbo].[T_Error]
 (
-	[ErrorNumber]		INT				NULL,
+    [ErrorNumber]       INT             NULL,
 
-	[ErrorSeverity]		INT				NULL,
+    [ErrorSeverity]     INT             NULL,
 
-	[ErrorState]		INT				NULL,
+    [ErrorState]        INT             NULL,
 
-	[ErrorProc]			NVARCHAR(200)	NULL,
+    [ErrorProc]         NVARCHAR(200)   NULL,
 
-	[ErrorLine]			INT				NULL,
+    [ErrorLine]         INT             NULL,
 
-	[ErrorMessage]		NVARCHAR(4000)	NULL,
+    [ErrorMessage]      NVARCHAR(4000)  NULL,
 
-	[ErrorTime]			DATETIME2		NULL,
+    [ErrorTime]         DATETIME2       NULL,
 )
 GO
 
@@ -87,14 +87,14 @@ GO
 -- ==========================================================================================
 CREATE TABLE [dbo].[T_TestAccount]
 (
-	[LoginId]			VARCHAR(50)		NOT NULL,						-- Test Account LoginId
+    [LoginId]           VARCHAR(50)     NOT NULL,                       -- Test Account LoginId
 
-	[RegTime]			DATETIME2		NOT	NULL DEFAULT(GETDATE()),	-- 등록 시간
+    [RegTime]           DATETIME2       NOT NULL DEFAULT(GETDATE()),    -- 등록 시간
 
-	CONSTRAINT [PK_T_TestAccount] PRIMARY KEY CLUSTERED
-	(
-		[LoginId] ASC
-	)
+    CONSTRAINT [PK_T_TestAccount] PRIMARY KEY CLUSTERED
+    (
+        [LoginId] ASC
+    )
 )
 GO
 
@@ -107,23 +107,23 @@ GO
 -- ==========================================================================================
 CREATE TABLE [dbo].[T_Account]
 (
-    [AccUid]			INT				IDENTITY(1,1),					-- T_Account Uid
+    [AccUid]            INT             IDENTITY(1,1),                  -- T_Account Uid
     
-    [LoginId]			VARCHAR(50)		NOT NULL,						-- Guest : UUID, Google : email, Ios : Game Center ID [ UUID : Universally Unique Identifire, 시간 & 공간을 이용하여 뽑아낸 128bit 중복되지 않는 값 ( for Unity SystemInfo.deviceUniqueIdentifier ) ]
+    [LoginId]           VARCHAR(50)     NOT NULL,                       -- Guest : UUID, Google : email, Ios : Game Center ID [ UUID : Universally Unique Identifire, 시간 & 공간을 이용하여 뽑아낸 128bit 중복되지 않는 값 ( for Unity SystemInfo.deviceUniqueIdentifier ) ]
     
-    [LoginPlatform]		TINYINT			NOT NULL,						-- Guest = 0, Google = 1, IOS = 2
+    [LoginPlatform]     TINYINT         NOT NULL,                       -- Guest = 0, Google = 1, IOS = 2
     
-    [LoginCount]		INT				NOT NULL DEFAULT(0),			-- 로그인 횟수
+    [LoginCount]        INT             NOT NULL DEFAULT(0),            -- 로그인 횟수
     
-    [SystemLanguage]	TINYINT			NOT NULL DEFAULT(0),			-- Input Language
+    [SystemLanguage]    TINYINT         NOT NULL DEFAULT(0),            -- Input Language
     
-    [Ban]				TINYINT			NOT NULL DEFAULT(0),			-- BAN 여부 [ 0 : 벤 아님, 1 : 불법 프로그램 사용, 2 : 욕설, 3 : 기타 ]
+    [Ban]               TINYINT         NOT NULL DEFAULT(0),            -- BAN 여부 [ 0 : 벤 아님, 1 : 불법 프로그램 사용, 2 : 욕설, 3 : 기타 ]
     
-    [BanEndTime]		SMALLDATETIME	NOT NULL DEFAULT(0),			-- BAN 일 경우 종료시간
+    [BanEndTime]        SMALLDATETIME   NOT NULL DEFAULT(0),            -- BAN 일 경우 종료시간
     
-    [CreatedTime]		DATETIME2		NOT NULL DEFAULT(GETDATE()),	-- 계정 생성 시간
+    [CreatedTime]       DATETIME2       NOT NULL DEFAULT(GETDATE()),    -- 계정 생성 시간
     
-    [LoginTime]			DATETIME2		NOT NULL DEFAULT(GETDATE()),	-- 로그인 시간
+    [LoginTime]         DATETIME2       NOT NULL DEFAULT(GETDATE()),    -- 로그인 시간
     
     CONSTRAINT [PK_T_Account] PRIMARY KEY CLUSTERED
     (
@@ -132,7 +132,7 @@ CREATE TABLE [dbo].[T_Account]
 )
 CREATE UNIQUE NONCLUSTERED INDEX [NIX_T_Account_LoginId] ON [dbo].[T_Account]
 (
-	[LoginId] ASC
+    [LoginId] ASC
 )
 GO
 
@@ -145,35 +145,35 @@ GO
 -- ==========================================================================================
 CREATE TABLE [dbo].[T_User]
 (
-    [AccUid]			INT				NOT NULL,						-- T_Account Uid
+    [AccUid]            INT             NOT NULL,                       -- T_Account Uid
     
-    [Nick]				NVARCHAR(8)	    NOT NULL,					    -- 닉네임 [ 유니코드 8자 까지, 중복 불가, 변경 가능 : 500 Jewel ]
+    [Nick]              NVARCHAR(8)     NOT NULL,                       -- 닉네임 [ 유니코드 8자 까지, 중복 불가, 변경 가능 : 500 Jewel ]
     
-    [Icon]				SMALLINT		NOT NULL DEFAULT(0),			-- 아이콘
+    [Icon]              SMALLINT        NOT NULL DEFAULT(0),            -- 아이콘
     
-    [Title]				SMALLINT		NOT NULL DEFAULT(0),			-- 칭호
+    [Title]             SMALLINT        NOT NULL DEFAULT(0),            -- 칭호
     
-    [Level]				INT				NOT NULL DEFAULT(1),			-- 유저 레벨
+    [Level]             INT             NOT NULL DEFAULT(1),            -- 유저 레벨
     
-    [Exp]				INT				NOT NULL DEFAULT(0),			-- 유저 경험치
+    [Exp]               INT             NOT NULL DEFAULT(0),            -- 유저 경험치
     
-    [Gold]				INT				NOT NULL DEFAULT(0),			-- 골드
+    [Gold]              INT             NOT NULL DEFAULT(0),            -- 골드
     
-    [Jewel]				INT				NOT NULL DEFAULT(0),			-- 보석
+    [Jewel]             INT             NOT NULL DEFAULT(0),            -- 보석
     
-    [HonorPT]			INT				NOT NULL DEFAULT(0),			-- 명예 포인트
+    [HonorPT]           INT             NOT NULL DEFAULT(0),            -- 명예 포인트
     
-    [GuildPT]			INT				NOT NULL DEFAULT(0),			-- 길드 포인트
+    [GuildPT]           INT             NOT NULL DEFAULT(0),            -- 길드 포인트
     
-    [ItemInventory]		SMALLINT		NOT NULL DEFAULT(60),			-- 인벤 갯수 [ Default 60개 ]
+    [ItemInventory]     SMALLINT        NOT NULL DEFAULT(60),           -- 인벤 갯수 [ Default 60개 ]
     
-    [Tutorial]			INT				NOT NULL DEFAULT(0),			-- 튜토리얼 State
+    [Tutorial]          INT             NOT NULL DEFAULT(0),            -- 튜토리얼 State
     
-    [StageId]			INT				NOT NULL DEFAULT(0),			-- 현재 진행 스테이지 Id [ From Server ]
+    [StageId]           INT             NOT NULL DEFAULT(0),            -- 현재 진행 스테이지 Id [ From Server ]
     
-    [GuildUid]			INT				NOT NULL DEFAULT(0),			-- T_Guild Primary Key
+    [GuildUid]          INT             NOT NULL DEFAULT(0),            -- T_Guild Primary Key
     
-    [InitTime]			DATETIME2		NOT NULL DEFAULT(GETDATE()),	-- 일일 초기화 진행된 시간 [ From Server ]
+    [InitTime]          DATETIME2       NOT NULL DEFAULT(GETDATE()),    -- 일일 초기화 진행된 시간 [ From Server ]
     
     CONSTRAINT [PK_T_User] PRIMARY KEY CLUSTERED
     (
@@ -195,7 +195,7 @@ GO
 -- ==========================================================================================
 CREATE TABLE [dbo].[T_Material]
 (
-    [AccUid]			INT				NOT NULL,						-- T_Account Uid
+    [AccUid]            INT             NOT NULL,                       -- T_Account Uid
 
     [Id]                TINYINT         NOT NULL,                       -- ID
 
@@ -218,21 +218,21 @@ GO
 -- ==========================================================================================
 CREATE TABLE [dbo].[T_Item]
 (
-	[AccUid]			INT				NOT NULL,						-- T_Account Uid
+    [AccUid]            INT             NOT NULL,                       -- T_Account Uid
 
-	[ItemUid]			INT				IDENTITY(1,1),					-- T_Item Uid
+    [ItemUid]           INT             IDENTITY(1,1),                  -- T_Item Uid
 
-	[Id]				INT				NOT NULL,						-- Item 테이블 ID
+    [Id]                INT             NOT NULL,                       -- Item 테이블 ID
 
-	[Enchant]			INT				NOT NULL DEFAULT(0),			-- 강화 수치
+    [Enchant]           INT             NOT NULL DEFAULT(0),            -- 강화 수치
 
-	[Count]				INT				NOT NULL DEFAULT(1),			-- 아이템 수량 [ 0개일경우 삭제된 아이템으로 판별 ]
+    [Count]             INT             NOT NULL DEFAULT(1),            -- 아이템 수량 [ 0개일경우 삭제된 아이템으로 판별 ]
 
-	CONSTRAINT [PK_T_Item] PRIMARY KEY CLUSTERED
-	(
-		[AccUid] ASC,
-		[ItemUid] ASC
-	)
+    CONSTRAINT [PK_T_Item] PRIMARY KEY CLUSTERED
+    (
+        [AccUid] ASC,
+        [ItemUid] ASC
+    )
 )
 GO
 
@@ -245,18 +245,18 @@ GO
 -- ==========================================================================================
 CREATE TABLE [dbo].[T_Key]
 (
-	[AccUid]			INT				NOT NULL,						-- T_Account Primary Key
+    [AccUid]            INT             NOT NULL,                       -- T_Account Primary Key
 
-	[Id]				TINYINT			NOT NULL,						-- 시간 재화 ID [ 21 : Stage ]
+    [Id]                TINYINT         NOT NULL,                       -- 시간 재화 ID [ 21 : Stage ]
 
-	[Count]				INT				NOT NULL DEFAULT(5),			-- 보유 수량
+    [Count]             INT             NOT NULL DEFAULT(5),            -- 보유 수량
 
-	[MaxTime]			DATETIME2		NOT NULL DEFAULT(GETDATE()),	-- 최대치로 충전되는 시간
+    [MaxTime]           DATETIME2       NOT NULL DEFAULT(GETDATE()),    -- 최대치로 충전되는 시간
 
-	CONSTRAINT [PK_T_Key] PRIMARY KEY CLUSTERED
-	(
-		[AccUid] ASC,
-		[Id] ASC
-	)
+    CONSTRAINT [PK_T_Key] PRIMARY KEY CLUSTERED
+    (
+        [AccUid] ASC,
+        [Id] ASC
+    )
 )
 GO
